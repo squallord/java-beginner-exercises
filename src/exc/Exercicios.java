4a079b7521eecaf5aa41d6ad9c532135dfa92308
Diego Marcílio <diego@localhost.localdomain>
2019-05-30 18:48:59 -0300

    unsolved exercises

--- src/exc/Exercicios.java
+++ src/exc/Exercicios.java
@@ -8,11 +8,7 @@ public abstract class Exercicios {
 	// Implemente uma função que lê uma lista não vazia e retorna o máximo inteiro positivo presente nela
 	public static int maxIntLista(int[] lista) {
 		// TODO
-		int max = lista[0];
-		for (int i = 0; i < lista.length; i++)
-			if (max < lista[i])
-				max = lista[i];
-		return max;
+		return 0;
 	}
 	
 	// Exercício 2:
@@ -20,10 +16,7 @@ public abstract class Exercicios {
 	// Exemplo: [1, 4, 2, 7, 3] deve retornar como [3, 7, 2, 4, 1]
 	public static int[] inverteLista(int[] lista) {
 		// TODO
-		int[] lista2 = new int[lista.length];
-		for(int i = 0; i < lista.length; i++)
-			lista2[i] = lista[lista.length - 1 - i];
-		return lista2;
+		return new int[0];
 	}
 	
 	// Exercício 3:
@@ -31,10 +24,7 @@ public abstract class Exercicios {
 	// Exemplo: [3, 7, 4, 1, 5, 9] -> deverá retornar 3 + 7 + 4 + 1 + 5 + 9 = 29 
 	public static int somaElementos(int[] lista) {
 		// TODO
-		int soma = 0;
-		for (int i = 0; i < lista.length; i++)
-			soma += lista[i];
-		return soma;
+		return 0;
 	}
 	
 	public static void main(String[] args) {
